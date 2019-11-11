395ad8efb571d87c993bbac0994e39130f53e484
Jairo Emanuel Galvez Cordova <jairo.jggc@gmail.com>
2019-11-10 22:49:24 -0500

    [s1] agregando progressBar

--- app/src/main/java/com/example/sunshinekotlin/MainActivity.kt
+++ app/src/main/java/com/example/sunshinekotlin/MainActivity.kt
@@ -8,27 +8,49 @@ import com.example.sunshinekotlin.utilities.NetworkUtils
 import android.os.AsyncTask
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
 import com.example.sunshinekotlin.data.SunshinePreferences
 
 class MainActivity : AppCompatActivity() {
 
     var mWeatherTextView: TextView? = null
+    var mErrorMessageDisplay: TextView? = null
+    var mLoadingIndicator: ProgressBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
         mWeatherTextView = findViewById(R.id.tv_weather_data)
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display)
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator)
         loadWeatherData()
     }
 
     private fun loadWeatherData() {
+        showWeatherDataView()
         val location = SunshinePreferences.getPreferredWeatherLocation(this)
         FetchWeatherTask().execute(location)
     }
 
+    private fun showWeatherDataView(){
+        mErrorMessageDisplay?.visibility = View.INVISIBLE
+        mWeatherTextView?.visibility = View.VISIBLE
+    }
+
+    private fun  showErrorMessage(){
+        mErrorMessageDisplay?.visibility = View.VISIBLE
+        mWeatherTextView?.visibility = View.INVISIBLE
+    }
+
     inner class FetchWeatherTask : AsyncTask<String, Void, List<String>>() {
 
-        override fun doInBackground(vararg params: String): List<String> {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            mLoadingIndicator?.visibility = View.VISIBLE
+        }
+
+        override fun doInBackground(vararg params: String): List<String>? {
 
             if (params.isEmpty()) {
                 return listOf()
@@ -45,14 +67,21 @@ class MainActivity : AppCompatActivity() {
 
             } catch (e: Exception) {
                 e.printStackTrace()
-                listOf()
+                null
             }
         }
 
-        override fun onPostExecute(weatherData: List<String>) {
-            for (weatherString in weatherData) {
-                mWeatherTextView?.append(weatherString + "\n\n\n")
+        override fun onPostExecute(weatherData: List<String>?) {
+            mLoadingIndicator?.visibility = View.INVISIBLE
+            if (weatherData != null) {
+                showWeatherDataView()
+                for (weatherString in weatherData) {
+                    mWeatherTextView?.append(weatherString + "\n\n\n")
+                }
+            }else{
+                showErrorMessage()
             }
+
         }
     }
 
