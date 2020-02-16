01ae07da49da8250cb1eed1deb3bf72de7d88393
Jairo Emanuel Galvez Cordova <jairo.jggc@gmail.com>
2020-02-15 21:48:34 -0500

    [s3] initialize sycronizacion

--- app/src/main/java/com/example/sunshinekotlin/data/WeatherDao.kt
+++ app/src/main/java/com/example/sunshinekotlin/data/WeatherDao.kt
@@ -1,12 +1,19 @@
 package com.example.sunshinekotlin.data
 
 import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface WeatherDao {
+
     @Query("SELECT * FROM weather ORDER BY date")
     fun loadAllWeathers(): LiveData<List<WeatherEntry>>
+
+    @Query("SELECT * FROM weather WHERE uid = :uid")
+    fun loadWeatherById(uid: Int): LiveData<WeatherEntry>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertWeather(weatherEntry: List<WeatherEntry>)
@@ -14,6 +21,6 @@ interface WeatherDao {
     @Query("DELETE FROM weather")
     fun deleteAll()
 
-    @Query("SELECT * FROM weather WHERE uid = :uid")
-    fun loadWeatherById(uid: Int): LiveData<WeatherEntry>
+    @Query("SELECT COUNT(uid) FROM weather")
+    fun getCount(): Int
 }
--- app/src/main/java/com/example/sunshinekotlin/sync/SunshineSyncUtils.kt
+++ app/src/main/java/com/example/sunshinekotlin/sync/SunshineSyncUtils.kt
@@ -2,9 +2,32 @@ package com.example.sunshinekotlin.sync
 
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
+import com.example.sunshinekotlin.data.AppDatabase
 
 
 object SunshineSyncUtils {
+
+    private var sInitialized = false
+
+    @Synchronized
+    fun initialize(context: Context) {
+
+        if (sInitialized) return
+
+        sInitialized = true
+
+        object : AsyncTask<Unit, Unit, Unit>() {
+            override fun doInBackground(vararg p0: Unit) {
+                val database = AppDatabase.getInstance(context)
+                val count = database.weatherDao().getCount()
+                if (count <= 0) {
+                    startImmediateSync(context)
+                }
+            }
+        }.execute()
+    }
+
     fun startImmediateSync(context: Context) {
         val intentToSyncImmediately = Intent(context, SunshineSyncIntentService::class.java)
         context.startService(intentToSyncImmediately)
