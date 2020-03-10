3bfd361e28e37570805a91269b9b9adc1afb8c9c
Jairo Emanuel Galvez Cordova <jairo.jggc@gmail.com>
2020-03-10 00:36:24 -0500

    [s3] detalle binding

--- app/src/main/java/com/example/sunshinekotlin/DetailActivity.kt
+++ app/src/main/java/com/example/sunshinekotlin/DetailActivity.kt
@@ -4,16 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sunshinekotlin.data.AppDatabase
 import com.example.sunshinekotlin.data.WeatherEntry
+import com.example.sunshinekotlin.databinding.ActivityDetailBinding
 import com.example.sunshinekotlin.model.WeatherViewModel
 import com.example.sunshinekotlin.model.WeatherViewModelFactory
 import com.example.sunshinekotlin.utilities.SunshineDateUtils.getFriendlyDateString
+import com.example.sunshinekotlin.utilities.SunshineWeatherUtils
 import com.example.sunshinekotlin.utilities.SunshineWeatherUtils.formatTemperature
 import com.example.sunshinekotlin.utilities.SunshineWeatherUtils.getFormattedWind
 import com.example.sunshinekotlin.utilities.SunshineWeatherUtils.getStringForWeatherCondition
@@ -24,25 +26,11 @@ class DetailActivity : AppCompatActivity() {
     private val FORECAST_SHARE_HASHTAG = " #SunshineApp"
     private var mForecastSummary: String? = null
 
-    private lateinit var mDateView: TextView
-    private lateinit var mDescriptionView: TextView
-    private lateinit var mHighTemperatureView: TextView
-    private lateinit var mLowTemperatureView: TextView
-    private lateinit var mHumidityView: TextView
-    private lateinit var mWindView: TextView
-    private lateinit var mPressureView: TextView
+    private lateinit var mDetailBinding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
-
-        mDateView = findViewById(R.id.date)
-        mDescriptionView = findViewById(R.id.weather_description)
-        mHighTemperatureView = findViewById(R.id.high_temperature)
-        mLowTemperatureView = findViewById(R.id.low_temperature)
-        mHumidityView = findViewById(R.id.humidity)
-        mWindView = findViewById(R.id.wind)
-        mPressureView = findViewById(R.id.pressure)
+        mDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
 
         if (intent.hasExtra(Intent.EXTRA_TEXT)) {
             val uid = intent.getIntExtra(Intent.EXTRA_TEXT,0)
@@ -72,34 +60,55 @@ class DetailActivity : AppCompatActivity() {
     }
 
     fun populateUI(weatherEntry: WeatherEntry){
+        val weatherId: Int = weatherEntry.weather_id
+
+        val weatherImageId: Int = SunshineWeatherUtils.getArtResourceForWeatherCondition(weatherId)
+
         val localDateMidnightGmt = weatherEntry.date.time
         val dateText = getFriendlyDateString(this, localDateMidnightGmt, true)
-        mDateView.text = dateText
 
-        val weatherId = weatherEntry.weather_id?:0
         val description = getStringForWeatherCondition(this, weatherId)
-        mDescriptionView.text = description
+        val descriptionA11y = getString(R.string.a11y_forecast, description)
 
         val highInCelsius = weatherEntry.max
         val highString = formatTemperature(this, highInCelsius)
-        mHighTemperatureView.text = highString
+        val highA11y = getString(R.string.a11y_high_temp, highString)
 
         val lowInCelsius =  weatherEntry.min
         val lowString = formatTemperature(this, lowInCelsius)
-        mLowTemperatureView.text = lowString
+        val lowA11y = getString(R.string.a11y_low_temp, lowString)
 
         val humidity = weatherEntry.humidity
         val humidityString = getString(R.string.format_humidity, humidity)
-        mHumidityView.text = humidityString
+        val humidityA11y = getString(R.string.a11y_humidity, humidityString)
 
         val windSpeed = weatherEntry.wind
         val windDirection = weatherEntry.degrees
         val windString = getFormattedWind(this, windSpeed, windDirection)
-        mWindView.text = windString
+        val windA11y = getString(R.string.a11y_wind, windString)
 
         val pressure = weatherEntry.pressure
         val pressureString = getString(R.string.format_pressure, pressure)
-        mPressureView.text = pressureString
+        val pressureA11y = getString(R.string.a11y_pressure, pressureString)
+
+        mDetailBinding.primaryInfo.weatherIcon.setImageResource(weatherImageId)
+        mDetailBinding.primaryInfo.date.text = dateText
+        mDetailBinding.primaryInfo.weatherDescription.text = description
+        mDetailBinding.primaryInfo.weatherDescription.contentDescription = descriptionA11y
+        mDetailBinding.primaryInfo.weatherIcon.contentDescription = descriptionA11y
+        mDetailBinding.primaryInfo.highTemperature.text = highString
+        mDetailBinding.primaryInfo.highTemperature.contentDescription = highA11y
+        mDetailBinding.primaryInfo.lowTemperature.text = lowString
+        mDetailBinding.primaryInfo.lowTemperature.contentDescription = lowA11y
+        mDetailBinding.extraDetails.humidity.text = humidityString
+        mDetailBinding.extraDetails.humidity.contentDescription = humidityA11y
+        mDetailBinding.extraDetails.humidityLabel.contentDescription = humidityA11y
+        mDetailBinding.extraDetails.windMeasurement.text = windString
+        mDetailBinding.extraDetails.windMeasurement.contentDescription = windA11y
+        mDetailBinding.extraDetails.windLabel.contentDescription = windA11y
+        mDetailBinding.extraDetails.pressure.text = pressureString;
+        mDetailBinding.extraDetails.pressure.contentDescription = pressureA11y;
+        mDetailBinding.extraDetails.pressureLabel.contentDescription = pressureA11y;
 
         mForecastSummary = String.format("%s - %s - %s/%s", dateText, description, highString, lowString)
     }
